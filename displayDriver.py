1da2bce6cb725bcb3dd4ed49600632d7de38fa1f
jabeywick <janith.abeywickrema@gmail.com>
2026-02-07 14:56:29 +0000

    Revert "Adjust image for small frame"
    This reverts commit c0570ddb46c902ecca39b0f3a7071b82bdb6dc90.

--- displayDriver.py
+++ displayDriver.py
@@ -1,17 +1,12 @@
 from PIL import Image, ImageEnhance, ImageOps
 from inky.auto import auto
 
-
 # Initialise display
 inky_display = auto()
 
-background = Image.new('RGB', (WIDTH, HEIGHT), (255, 255, 255))
-
-offset_x = (inky_display.width - background.width) // 2
-offset_y = (inky_display.height - background.height) // 2
-
 # Open and show screenshot
 image = Image.open("screenshot.png")
+image = image.rotate(90, expand=True)
 
 converter = ImageEnhance.Color(image)
 image = converter.enhance(2.0)
@@ -19,12 +14,8 @@ image = converter.enhance(2.0)
 contrast = ImageEnhance.Contrast(image)
 image = converter.enhance(1.2)
 
-
-background.paste(image, (offset_x, offset_y))
-imageToDisplay = background.rotate(90, expand=True)
-
 # Limit colors to be handled on eink display better
-ImageOps.posterize(imageToDisplay, bits=4)
+image = ImageOps.posterize(image, bits=4)
 
-inky_display.set_image(imageToDisplay)
+inky_display.set_image(image)
 inky_display.show()
